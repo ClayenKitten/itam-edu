b027e51579cc54a6955dedf4189f8b0c46a03397
ClayenKitten <me@clayenkitten.dev>
2025-01-02 19:52:47 +0300

    fix(web): make lesson always defined in lesson's layout load function

--- packages/web/src/routes/(edu)/c/[year]/[[semester=semester]]/[course]/[lesson]/+layout.server.ts
+++ packages/web/src/routes/(edu)/c/[year]/[[semester=semester]]/[course]/[lesson]/+layout.server.ts
@@ -5,26 +5,23 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async ({ fetch, params, parent }) => {
     const { course } = await parent();
 
-    let lesson = undefined;
-    if (params.year && params.course) {
-        // Lookup lesson id
-        const lessonIdResp = await api({ fetch }).courses[
-            ":course"
-        ].lessons.lookup[":lessonSlug"].$get({
-            param: { course: course.id, lessonSlug: params.lesson }
-        });
-        if (lessonIdResp.status === 404) error(404);
-        const { id: lessonId } = await lessonIdResp.json();
+    // Lookup lesson id
+    const lessonIdResp = await api({ fetch }).courses[":course"].lessons.lookup[
+        ":lessonSlug"
+    ].$get({
+        param: { course: course.id, lessonSlug: params.lesson }
+    });
+    if (lessonIdResp.status === 404) error(404);
+    const { id: lessonId } = await lessonIdResp.json();
 
-        // Get lesson
-        const lessonResp = await api({ fetch }).courses[":course"].lessons[
-            ":lesson"
-        ].$get({
-            param: { course: course.id, lesson: lessonId }
-        });
-        if (lessonResp.status === 404) error(404);
-        lesson = await lessonResp.json();
-    }
+    // Get lesson
+    const lessonResp = await api({ fetch }).courses[":course"].lessons[
+        ":lesson"
+    ].$get({
+        param: { course: course.id, lesson: lessonId }
+    });
+    if (lessonResp.status === 404) error(404);
+    let lesson = await lessonResp.json();
 
     return { lesson };
 };
