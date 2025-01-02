f3764d7f508d54d416904915cb0566891cf74053
ClayenKitten <me@clayenkitten.dev>
2025-01-02 19:58:51 +0300

    fix(web): incorrect env typing

--- packages/web/src/lib/api.ts
+++ packages/web/src/lib/api.ts
@@ -6,8 +6,8 @@ import type { AppType } from "itam-edu-api";
 
 export default function api(init: { fetch: Window["fetch"] }) {
     const baseUrl = browser
-        ? public_env.PUBLIC_ITAM_EDU_WEB_API_URL
-        : private_env.ITAM_EDU_WEB_API_URL;
+        ? public_env.PUBLIC_ITAM_EDU_WEB_API_URL!
+        : private_env.ITAM_EDU_WEB_API_URL!;
     const client = hc<AppType>(baseUrl, { fetch: init.fetch });
     return client;
 }
