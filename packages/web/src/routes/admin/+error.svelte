61d0e7be9f696d0a02ce6a79ac6f03f5a2f63fb5
ClayenKitten <me@clayenkitten.dev>
2025-01-04 11:00:45 +0300

    feat(web): implement error page

---
+++ packages/web/src/routes/admin/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import { getReasonPhrase } from "http-status-codes";
+</script>
+
+<div class="flex flex-col justify-center items-center w-full h-full p-5">
+    <span class="text-3xl text-text font-bold">{page.status}</span>
+    <span class="text-xl text-text">{getReasonPhrase(page.status)}</span>
+    <button
+        class="bg-primary h-12 mt-8 px-4 text-text font-bold rounded-full"
+        onclick={() => {
+            history.back();
+        }}
+    >
+        Go back
+    </button>
+</div>
