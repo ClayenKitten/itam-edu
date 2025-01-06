c9d7d30434e7389dd0312fbb3ce644f5575f75fc
ClayenKitten <me@clayenkitten.dev>
2025-01-07 02:25:54 +0300

    fix(web): use `PUBLIC_` for server API URL to avoid issue with Svelte safeguarding private env import

--- packages/web/src/lib/api.ts
+++ packages/web/src/lib/api.ts
@@ -1,13 +1,12 @@
 import { browser } from "$app/environment";
 import { env as public_env } from "$env/dynamic/public";
-import { env as private_env } from "$env/dynamic/private";
 import { hc } from "hono/client";
 import type { AppType } from "itam-edu-api";
 
 export default function api(init: { fetch: Window["fetch"] }) {
     const baseUrl = browser
-        ? public_env.PUBLIC_ITAM_EDU_WEB_API_URL!
-        : private_env.ITAM_EDU_WEB_API_URL!;
+        ? public_env.PUBLIC_ITAM_EDU_WEB_API_URL_BROWSER!
+        : public_env.PUBLIC_ITAM_EDU_WEB_API_URL_SERVER!;
     const client = hc<AppType>(baseUrl, { fetch: init.fetch });
     return client;
 }
