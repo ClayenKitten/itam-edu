1000596fbc00c50509e7a3bff7f70ff3028a2dbc
ClayenKitten <me@clayenkitten.dev>
2025-01-09 00:37:39 +0300

    feat(api): add CORS support

--- packages/api/src/global.d.ts
+++ packages/api/src/global.d.ts
@@ -1,6 +1,8 @@
 namespace NodeJS {
     interface ProcessEnv {
         NODE_ENV: "production" | "development";
+        ITAM_EDU_API_HOST: string;
         ITAM_EDU_API_PORT: string;
+        ITAM_EDU_API_DB_CONNECTION_STRING: string;
     }
 }
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -8,23 +8,31 @@ import { userService } from "./services/user.js";
 import logger, { loggerMiddleware } from "./logger.js";
 import { bodyLimit } from "hono/body-limit";
 import createContext, { type AppEnv } from "./ctx.js";
+import { cors } from "hono/cors";
 
-const hostname = "0.0.0.0";
 const port = Number(env.ITAM_EDU_API_PORT) ?? 3000;
 
 const app = await createApp();
 
 export async function createApp() {
     return new Hono<AppEnv>()
-        .use(bodyLimit({ maxSize: 50 * 1024 * 1024 }))
         .use(loggerMiddleware())
+        .use(bodyLimit({ maxSize: 50 * 1024 * 1024 }))
+        .use(
+            cors({
+                origin:
+                    env.NODE_ENV === "development"
+                        ? "*"
+                        : env.ITAM_EDU_API_HOST!
+            })
+        )
         .use(createContext)
         .route("/courses", await courseService())
         .route("/users", await userService())
         .get("/healthz", c => c.body(null, 200));
 }
 
-const server = serve({ fetch: app.fetch, hostname, port }, info => {
+const server = serve({ fetch: app.fetch, hostname: "0.0.0.0", port }, info => {
     logger.notice(`Listening at http://${info.address}:${info.port}`);
 });
 
