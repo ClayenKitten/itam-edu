5f8a1a4d96d9e5640680025dbb1f199106522573
ClayenKitten <me@clayenkitten.dev>
2025-01-11 18:21:31 +0300

    feat(api): add database errors logging

--- packages/api/src/db.ts
+++ packages/api/src/db.ts
@@ -2,14 +2,25 @@ import type { DB } from "itam-edu-db";
 import pg from "pg";
 import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely";
 import { env } from "process";
+import logger from "./logger.js";
 
 export default function createDatabaseConnection() {
     return new Kysely<DB>({
         dialect: new PostgresDialect({
             pool: new pg.Pool({
-                connectionString: env.ITAM_EDU_API_DB_CONNECTION_STRING
+                connectionString: env.ITAM_EDU_API_DB_CONNECTION_STRING,
+                log: () => {}
             })
         }),
-        plugins: [new CamelCasePlugin()]
+        plugins: [new CamelCasePlugin()],
+        log: e => {
+            if (e.level === "error") {
+                logger.error("Database Query Failed", {
+                    error: `${e.error}`,
+                    query: e.query.sql,
+                    stacktrace: Error.captureStackTrace({})
+                });
+            }
+        }
     });
 }
