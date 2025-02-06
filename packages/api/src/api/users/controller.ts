3d713a7ce5649a2edd78f6b57e0b7989fa44f354
ClayenKitten <me@clayenkitten.dev>
2025-02-06 11:00:49 +0300

    chore: remove unused login endpoint

--- packages/api/src/api/users/controller.ts
+++ packages/api/src/api/users/controller.ts
@@ -33,46 +33,5 @@ export async function userController<PREFIX extends string>(prefix: PREFIX) {
                     code: t.String({ minLength: 0, maxLength: 128 })
                 })
             }
-        )
-        .post(
-            "/loginAttempt",
-            async ({ db, body, error }) => {
-                const codeLength = 8;
-                const codeRadix = 16;
-                const code = randomInt(0, codeRadix ** codeLength)
-                    .toString(codeRadix)
-                    .padStart(codeLength, "0")
-                    .toUpperCase();
-
-                const expiresAfterMinutes = 5;
-                const expires = new Date(
-                    new Date().getTime() + expiresAfterMinutes * 60000
-                );
-
-                const success = await db.user.createLoginAttempt({
-                    code,
-                    expires,
-                    ...body
-                });
-
-                if (success) {
-                    return { code, expires };
-                } else {
-                    return error(500);
-                }
-            },
-            {
-                detail: {
-                    description:
-                        "Creates login attempt for user if it exists, and creates user otherwise.",
-                    tags: ["Users", "Telegram Bot"]
-                },
-                authenticateIntegration: "telegram",
-                body: t.Object({
-                    tgUserId: t.String(),
-                    tgChatId: t.String(),
-                    tgUsername: t.String()
-                })
-            }
         );
 }
