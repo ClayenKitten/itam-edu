afe5218336abbce31ad86d019f4d62ea52133dd1
ClayenKitten <me@clayenkitten.dev>
2025-03-09 09:17:23 +0300

    feat(api): connect to Redis via BullMQ

--- packages/api/src/global.d.ts
+++ packages/api/src/global.d.ts
@@ -4,6 +4,7 @@ namespace NodeJS {
         ITAM_EDU_API_HOST: string;
         ITAM_EDU_API_PORT: string;
         ITAM_EDU_API_DB_CONNECTION_STRING: string;
+        ITAM_EDU_API_REDIS_CONNECTION_STRING: string;
         ITAM_EDU_API_WEB_HOST: string;
         ITAM_EDU_API_TG_TOKEN: string;
         ITAM_EDU_API_TG_SUPPORT_USERNAME: string;
