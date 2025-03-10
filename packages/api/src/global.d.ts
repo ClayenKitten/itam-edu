9cd071ae962ada64f105c219c6bb665850a722ba
ClayenKitten <me@clayenkitten.dev>
2025-03-10 07:27:23 +0300

    feat(infra): merge configuration
    Use common `.env` file for all services.

--- packages/api/src/global.d.ts
+++ packages/api/src/global.d.ts
@@ -1,11 +1,11 @@
 namespace NodeJS {
     interface ProcessEnv {
         NODE_ENV: "production" | "development";
-        ITAM_EDU_API_HOST: string;
+        ITAM_EDU_FRONTEND_URL: string;
+        ITAM_EDU_API_DOMAIN: string;
         ITAM_EDU_API_PORT: string;
         ITAM_EDU_API_DB_CONNECTION_STRING: string;
         ITAM_EDU_API_REDIS_CONNECTION_STRING: string;
-        ITAM_EDU_API_WEB_HOST: string;
         ITAM_EDU_API_TG_TOKEN: string;
         ITAM_EDU_API_TG_SUPPORT_USERNAME: string;
     }
