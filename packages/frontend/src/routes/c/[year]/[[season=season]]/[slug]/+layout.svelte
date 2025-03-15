896899dd6312d698a7fb9c91750090b84d678008
ClayenKitten <me@clayenkitten.dev>
2025-03-15 10:55:49 +0300

    feat(frontend): update header to match design and add login button for unauthenticated users

--- packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/+layout.svelte
+++ packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div
     id="wrapper"
-    class="grid grid-cols-[278px_1fr] grid-rows-[60px_1fr] min-h-dvh bg-background"
+    class="grid grid-cols-[278px_1fr] grid-rows-[56px_1fr] min-h-dvh bg-background"
     style:--color-primary={data.course.colorPrimary}
     style:--color-on-primary={data.course.colorOnPrimary}
 >
@@ -15,25 +15,28 @@
             x => x.courseId === data.course.id
         ) ?? false}
     />
-    <header class="flex justify-end px-7 py-1.25 gap-3">
+    <header class="flex justify-end px-7 py-2 gap-2.5">
         <button
-            class="h-full aspect-square bg-on-primary rounded-xs"
+            class="flex justify-center items-center h-full aspect-square hover:bg-on-primary rounded-xs"
             aria-label="Уведомления"
         >
             <i class="ph ph-bell text-primary text-[20px]"></i>
         </button>
-        <a
-            href="/calendar"
-            class="flex justify-center items-center h-full aspect-square bg-on-primary rounded-xs"
+        <button
+            class="flex justify-center items-center h-full aspect-square hover:bg-on-primary rounded-xs"
             aria-label="Календарь"
         >
             <i class="ph ph-calendar-dots text-primary text-[20px]"></i>
-        </a>
-        <a
-            href="/profile"
-            class="flex justify-center items-center h-full aspect-square bg-primary rounded-xs"
-            aria-label="Профиль"
-        ></a>
+        </button>
+        {#if data.user}
+            <a
+                href="/profile"
+                class="flex justify-center items-center h-full aspect-square ml-2.5 bg-primary rounded-xs"
+                aria-label="Профиль"
+            ></a>
+        {:else}
+            <button class="btn h-full ml-2.5">Войти</button>
+        {/if}
     </header>
     <main class="flex flex-col items-stretch">
         {@render children()}
