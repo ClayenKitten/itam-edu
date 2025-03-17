640ff9a30d770d9a44aee734562ccbd447bc21e7
ClayenKitten <me@clayenkitten.dev>
2025-03-18 02:24:00 +0300

    feat(frontend): implement basic error page for course layout

---
+++ packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
+<div class="flex flex-col justify-center items-center w-full h-full gap-8">
+    <h1>Ошибка {page.status}</h1>
+    <p class="text-h3">{page.error?.message}</p>
+</div>
