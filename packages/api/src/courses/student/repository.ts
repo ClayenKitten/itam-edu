7759977705fb4d57ea759893840312bfd57110af
AlexThatDefinetlyStudiesIT <m2208273@edu.misis.ru>
2025-03-27 11:42:31 +0300

    feat: student enrollment and expelling

--- packages/api/src/courses/student/repository.ts
+++ packages/api/src/courses/student/repository.ts
@@ -1,6 +1,7 @@
 import { Repository } from "../../db/repository";
 import { schemaFields } from "../../util";
 import * as userSchema from "../../users/schema";
+import * as schema from "./schema";
 
 export default class StudentRepository extends Repository {
     public async getAll(
@@ -14,5 +15,34 @@ export default class StudentRepository extends Repository {
             .select(schemaFields(userSchema.user))
             .execute();
         return students;
+    }
+
+    public async addStudent(
+        courseId: string,
+        userId: string
+    ): Promise<typeof schema.student.static | null> {
+        const newStudent = await this.db
+            .insertInto("courseStudents")
+            .values({
+                courseId: courseId,
+                userId: userId
+            })
+            .onConflict(cb => cb.doNothing())
+            .returning(schemaFields(schema.student))
+            .executeTakeFirst();
+        if (!newStudent) return null;
+        return newStudent;
+    }
+
+    public async expelStudent(
+        courseId: string,
+        userId: string
+    ): Promise<boolean> {
+        const result = await this.db
+            .deleteFrom("courseStudents")
+            .where("courseId", "=", courseId)
+            .where("userId", "=", userId)
+            .executeTakeFirst();
+        return result.numDeletedRows > 0n;
     }
 }
