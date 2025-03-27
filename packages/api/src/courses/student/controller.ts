bf50023d6a04c45e86cc7ca66148c9786a490632
AlexThatDefinetlyStudiesIT <m2208273@edu.misis.ru>
2025-03-27 11:42:31 +0300

    feat: student enrollment and expelling

--- packages/api/src/courses/student/controller.ts
+++ packages/api/src/courses/student/controller.ts
@@ -5,7 +5,10 @@ export function studentController<PREFIX extends string>(prefix: PREFIX) {
     return new Elysia({ name: "students", prefix, tags: ["Students"] })
         .use(initContext())
         .guard({
-            params: t.Object({ course: t.String({ format: "uuid" }) })
+            params: t.Object(
+                { course: t.String({ format: "uuid" }) },
+                { additionalProperties: true }
+            )
         })
         .get(
             "",
@@ -16,9 +19,69 @@ export function studentController<PREFIX extends string>(prefix: PREFIX) {
             },
             {
                 requireAuthentication: true,
+                params: t.Object({ course: t.String({ format: "uuid" }) }),
                 detail: {
                     summary: "List students",
                     description: "Returns all students enrolled to the course."
+                }
+            }
+        )
+        .post(
+            "/:student",
+            async ({ db, user, params, error }) => {
+                if (
+                    user.id !== params.student &&
+                    !user.isCourseStaff(params.course)
+                ) {
+                    return error(403);
+                }
+                const student = await db.student.addStudent(
+                    params.course,
+                    params.student
+                );
+                if (!student)
+                    return error(
+                        409,
+                        "User is already enrolled to the course."
+                    );
+                return student;
+            },
+            {
+                requireAuthentication: true,
+                params: t.Object({
+                    course: t.String({ format: "uuid" }),
+                    student: t.String({ format: "uuid" })
+                }),
+                detail: {
+                    summary: "Enroll student",
+                    description: "Enrolls student in the course."
+                }
+            }
+        )
+        .delete(
+            "/:student",
+            async ({ db, user, params, error }) => {
+                if (
+                    user.id !== params.student &&
+                    !user.isCourseStaff(params.course)
+                ) {
+                    return error(403);
+                }
+                const success = await db.student.expelStudent(
+                    params.course,
+                    params.student
+                );
+                if (!success) return error(404);
+            },
+            {
+                requireAuthentication: true,
+                params: t.Object({
+                    course: t.String({ format: "uuid" }),
+                    student: t.String({ format: "uuid" })
+                }),
+                detail: {
+                    summary: "Expel student",
+                    description: "Expels a specified student from the course."
                 }
             }
         );
