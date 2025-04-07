34f4fd47385c2e0db626f683a5f1e2798d7e1c76
domik <113109474+pathfinder-22@users.noreply.github.com>
2025-04-07 03:13:35 +0300

    feat(frontend): display homework tags

---
+++ packages/frontend/src/lib/components/Tag.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" module>
+    export type TagKind = "new" | "rejected" | "submitted" | "accepted";
+</script>
+
+<script lang="ts">
+    export let kind: TagKind;
+</script>
+
+{#if kind === "new"}
+    <span
+        class="py-1.5 px-3.5 rounded-2xs bg-[#ECEEFF] text-[#6E7CEC] text-[14px]"
+    >
+        Новое
+    </span>
+{:else if kind === "rejected"}
+    <span
+        class="py-1.5 px-3.5 rounded-2xs bg-[#FFEDEA] text-[#FF6A55] text-[14px]"
+    >
+        Отклонено
+    </span>
+{:else if kind === "submitted"}
+    <span
+        class="py-1.5 px-3.5 rounded-2xs bg-[#E7F4FB] text-[#2462D6] text-[14px]"
+    >
+        На проверке
+    </span>
+{:else}
+    <span
+        class="py-1.5 px-3.5 rounded-2xs bg-[#E9FBE7] text-[#36D624] text-[14px]"
+    >
+        Принято
+    </span>
+{/if}
--- packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/homeworks/+page.svelte
+++ packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/homeworks/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+    import Tag, { type TagKind } from "$lib/components/Tag.svelte";
     import { coursePath } from "$lib/path.js";
     import { format as formatDate } from "date-fns";
 
     let { data } = $props();
+
+    const getTagKind = (homeworkId: string): TagKind => {
+        const homeworkSubmissions = data.submissions.filter(
+            submission => submission.homework === homeworkId
+        );
+        const submission = homeworkSubmissions.at(-1);
+        if (!submission) return "new";
+        if (!submission.review) return "submitted";
+        return submission.review.accepted ? "accepted" : "rejected";
+    };
 </script>
 
 <div class="flex flex-col gap-10 p-10">
@@ -16,11 +27,12 @@
                 href={`${coursePath(data.course)}/homeworks/${homework.id}`}
             >
                 <div class="flex flex-col gap-3">
-                    <header>
+                    <header class="flex items-center gap-3">
                         <h4>{homework.title}</h4>
+                        <Tag kind={getTagKind(homework.id)} />
                     </header>
-                    {#if homework.deadline}
-                        <p class="text-on-surface-contrast opacity-50">
+                    <p class="text-on-surface-contrast opacity-50">
+                        {#if homework.deadline}
                             <span>До</span>
                             <span>
                                 {formatDate(
@@ -28,8 +40,10 @@
                                     "dd.MM.yy / HH:mm"
                                 )}
                             </span>
-                        </p>
-                    {/if}
+                        {:else}
+                            Без дедлайна
+                        {/if}
+                    </p>
                 </div>
                 <i class="ph ph-caret-right text-[26px] self-center"></i>
             </a>
