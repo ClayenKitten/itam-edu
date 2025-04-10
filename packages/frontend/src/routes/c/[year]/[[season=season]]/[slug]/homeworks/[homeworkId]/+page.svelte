8d861a4a760f9d0b102536454dccb817d9e0db5d
ClayenKitten <me@clayenkitten.dev>
2025-04-10 23:22:24 +0000

    feat(frontend): implement homework submission chat

--- packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/homeworks/[homeworkId]/+page.svelte
+++ packages/frontend/src/routes/c/[year]/[[season=season]]/[slug]/homeworks/[homeworkId]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
+    import { goto, invalidate } from "$app/navigation";
+    import api from "$lib/api.js";
     import IconButton from "$lib/components/IconButton.svelte";
     import TipTap from "$lib/components/TipTap.svelte";
     import { coursePath } from "$lib/path.js";
@@ -10,10 +11,33 @@
     const canEdit =
         data.permissions?.course.find(x => x.courseId === data.course.id)
             ?.permissions.canEditContent === true;
+    const canReview =
+        data.permissions?.course.find(x => x.courseId === data.course.id)
+            ?.permissions.canManageSubmissions === true;
+    const isStudent = data.enrollments?.some(
+        e => e.courseId === data.course.id
+    );
 
-    async function save() {
-        // TODO
+    async function sendMessage() {
+        if (!data.submission) return;
+        const response = await api({ fetch })
+            .courses({ course: data.course.id })
+            .submissions({
+                homework: data.homework.id
+            })({ student: data.submission.studentId })
+            .post({
+                content: newMessageText,
+                accepted: isStudent ? null : newMessageAccepted
+            });
+        if (response.error) alert(response.error.status);
+        else {
+            newMessageText = "";
+            await invalidate("app:submission");
+            await invalidate("app:submissions");
+        }
     }
+    let newMessageText = $state("");
+    let newMessageAccepted = $state(true);
 </script>
 
 <div
@@ -34,7 +58,7 @@
         </a>
         <header class="flex flex-col gap-2">
             <h2>{data.homework.title}</h2>
-            <h4>
+            <h4 class="text-on-surface-muted">
                 {#if data.homework.deadline}
                     До
                     {formatDate(data.homework.deadline, "dd.MM.yy / HH:mm")}
@@ -77,28 +101,120 @@
             </footer>
         {/if}
     </section>
-    <section class="grow-1 flex flex-col"></section>
-    <label
-        class={[
-            "flex items-center gap-3 w-full h-15 py-1 px-5 text-comment text-on-surface-muted",
-            "bg-surface border border-on-primary focus-within:border-primary rounded-sm shadow"
-        ]}
-    >
-        <input
-            class="h-full w-full outline-0"
-            placeholder="Напишите сообщение..."
-        />
-        <button
-            class="group h-7 w-7 outline-0"
-            aria-label="Отправить"
-            title="Отправить"
+    {#if (isStudent || canReview) && data.submission}
+        <section class="grow-1 flex flex-col-reverse gap-7.5">
+            {#each data.submission.messages.toReversed() as message}
+                {@const user =
+                    message.userId === null
+                        ? null
+                        : (data.submission.users.find(
+                              u => u.id === message.userId
+                          ) ?? null)}
+                {#if user}
+                    <li
+                        class={[
+                            "relative flex flex-col gap-5 py-5.5 px-6.5 w-[min(100%,600px)]",
+                            "rounded-xl shadow first-of-type:mb-auto",
+                            (user.id === data.submission.studentId) !==
+                            isStudent
+                                ? "self-end bg-surface"
+                                : "self-start bg-on-primary"
+                        ]}
+                    >
+                        {#if message.accepted !== null}
+                            <div
+                                class={[
+                                    "absolute top-[calc(50%-16px)] ",
+                                    isStudent ? "-left-4" : "-right-4"
+                                ]}
+                            >
+                                {@render acceptedIcon(message.accepted)}
+                            </div>
+                        {/if}
+                        <header class="flex items-center gap-3">
+                            <div
+                                class="flex justify-center items-center h-10 w-10 bg-primary rounded-xs"
+                            >
+                                {#if user.avatar}
+                                    <img
+                                        class="rounded-xs"
+                                        src={user.avatar}
+                                        alt=""
+                                    />
+                                {:else}
+                                    <span class="text-on-primary text-comment">
+                                        {user.tgUsername[0]}
+                                    </span>
+                                {/if}
+                            </div>
+                            <span>
+                                {#if !user.firstName && !user.lastName}
+                                    {user.tgUsername}
+                                {:else}
+                                    {user.firstName}
+                                    {user.lastName}
+                                {/if}
+                            </span>
+                        </header>
+                        <div class="shrink-0 break-words text-balance">
+                            {message.content}
+                        </div>
+                        <footer class="text-date text-on-surface-muted">
+                            {formatDate(message.sentAt, "dd.MM.yy HH:mm")}
+                        </footer>
+                    </li>
+                {/if}
+            {/each}
+        </section>
+        <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+        <label
+            class={[
+                "flex items-center gap-3 w-full min-h-15 px-5 text-comment text-on-surface-muted",
+                "bg-surface border border-on-primary focus-within:border-primary rounded-sm shadow"
+            ]}
+            onkeydown={e => {
+                if (newMessageText.length === 0) return;
+                if (e.key !== "Enter") return;
+                sendMessage();
+            }}
         >
-            <i
-                class="group-hover:hidden group-focus:hidden ph ph-paper-plane-right text-[28px] text-primary"
-            ></i>
-            <i
-                class="not-group-hover:not-group-focus:hidden ph-fill ph-paper-plane-right text-[28px] text-primary"
-            ></i>
-        </button>
-    </label>
+            <input
+                class="grow-1 text-on-surface-contrast h-full outline-0"
+                bind:value={newMessageText}
+                placeholder="Напишите сообщение..."
+            />
+            {#if canReview}
+                <button
+                    onclick={() => (newMessageAccepted = !newMessageAccepted)}
+                >
+                    {@render acceptedIcon(newMessageAccepted)}
+                </button>
+            {/if}
+            <button
+                class="group h-7 w-7 outline-0"
+                aria-label="Отправить"
+                title="Отправить"
+                disabled={newMessageText.length === 0}
+                onclick={sendMessage}
+            >
+                <i
+                    class="group-hover:hidden group-focus:hidden ph ph-paper-plane-right text-[28px] text-primary"
+                ></i>
+                <i
+                    class="not-group-hover:not-group-focus:hidden ph-fill ph-paper-plane-right text-[28px] text-primary"
+                ></i>
+            </button>
+        </label>
+    {/if}
 </div>
+
+{#snippet acceptedIcon(accepted: boolean)}
+    <i
+        class={[
+            "flex justify-center items-center h-8 w-8 rounded-full",
+            accepted
+                ? "ph ph-thumbs-up bg-[#469c3c] text-[white]"
+                : "ph ph-thumbs-down bg-[#c94634] text-[white]"
+        ]}
+    ></i>
+{/snippet}
