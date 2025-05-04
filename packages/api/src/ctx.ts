fac07acd64e0f0a6b71eafd8d8405f5d072eab03
ClayenKitten <me@clayenkitten.dev>
2025-05-04 01:27:18 +0000

    refactor(api): move database creation into the context directly

--- packages/api/src/ctx.ts
+++ packages/api/src/ctx.ts
@@ -1,9 +1,20 @@
 import NotificationService from "./notifications/service";
 import type { AppConfig } from "./config";
-import { createDatabaseContext } from "./db";
 import createDatabaseConnection from "./db/connection";
 import { LessonService } from "./courses/lesson/service";
 import { SubmissionService } from "./courses/submission/service";
+
+import UserRepository from "./users/repository";
+import CourseRepository from "./courses/repository";
+import LessonRepository from "./courses/lesson/repository";
+import StudentRepository from "./courses/student/repository";
+import StaffRepository from "./staff/repository";
+import NotificationRepository from "./notifications/repository";
+import HomeworkRepository from "./courses/homework/repository";
+import SubmissionRepository from "./courses/submission/repository";
+import { LessonQuery } from "./courses/lesson/query";
+import { HomeworksQuery } from "./courses/homework/query";
+import { SubmissionQuery } from "./courses/submission/query";
 
 /** Global context of the application. */
 export type AppContext = ReturnType<typeof createAppContext>;
@@ -14,7 +25,19 @@ export function createAppContext(config: AppConfig) {
         config.postgres.connectionString
     );
 
-    const db = createDatabaseContext(databaseConnection);
+    const db = {
+        user: new UserRepository(databaseConnection),
+        course: new CourseRepository(databaseConnection),
+        lesson: new LessonRepository(databaseConnection),
+        lessonQuery: new LessonQuery(databaseConnection),
+        homework: new HomeworkRepository(databaseConnection),
+        homeworksQuery: new HomeworksQuery(databaseConnection),
+        student: new StudentRepository(databaseConnection),
+        staff: new StaffRepository(databaseConnection),
+        notification: new NotificationRepository(databaseConnection),
+        submission: new SubmissionRepository(databaseConnection),
+        submissionQuery: new SubmissionQuery(databaseConnection)
+    };
 
     const notification = new NotificationService(
         config.redis,
