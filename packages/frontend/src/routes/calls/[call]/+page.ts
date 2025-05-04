7a2f65ab93f9fb8f21243411c6a50099ab0d91a1
ClayenKitten <me@clayenkitten.dev>
2025-05-04 20:56:13 +0000

    feat(frontend): add basic calls support

---
+++ packages/frontend/src/routes/calls/[call]/+page.ts
@@ -0,0 +1,17 @@
+import type { PageLoad } from "./$types";
+import api from "$lib/api";
+import { error } from "@sveltejs/kit";
+
+export const load: PageLoad = async ({ fetch, params, depends }) => {
+    depends("app:call");
+
+    const call = await getCall(fetch, params.call);
+
+    return { call };
+};
+
+async function getCall(fetch: typeof window.fetch, call: string) {
+    const response = await api({ fetch }).calls({ call }).get();
+    if (response.error) error(response.status);
+    return response.data;
+}
---
+++ packages/frontend/src/routes/calls/[call]/ParticipantAudio.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { AudioTrackState } from "./state.svelte";
+
+    let { track }: Props = $props();
+
+    type Props = {
+        track: AudioTrackState;
+    };
+
+    onMount(() => {
+        track.attach(audio);
+    });
+
+    let audio: HTMLAudioElement;
+</script>
+
+<audio class="hidden" bind:this={audio}></audio>
