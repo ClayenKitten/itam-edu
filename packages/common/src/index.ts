e06dfc281c35561554f599036649876725c1e9ca
ClayenKitten <me@clayenkitten.dev>
2025-05-09 02:40:35 +0000

    chore(common): add queue names and types

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./user";
 export * from "./files";
+export * as queues from "./queues";
---
+++ packages/common/src/queues/telegram.ts
@@ -0,0 +1,18 @@
+/** User sent a message to the bot in a private chat. */
+export type InboundPrivateMessage = {
+    text: string;
+    sender: {
+        id: string;
+        firstName: string;
+        lastName: string | null;
+        username: string;
+    };
+};
+export const INBOUND_PRIVATE_MESSAGE_QUEUE = "tg.inbound.private-message";
+
+/** Server sent a message to a private chat. */
+export type OutboundPrivateMessage = {
+    chatId: string;
+    text: string;
+};
+export const OUTBOUND_PRIVATE_MESSAGE_QUEUE = "tg.outbound.message";
