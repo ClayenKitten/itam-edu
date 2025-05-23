ac2a594468679f7c52429aaa951f649e3ada6830
ClayenKitten <me@clayenkitten.dev>
2025-05-23 19:09:46 +0000

    refactor(api): cleanup env access

--- packages/api/src/api/plugins/cors.ts
+++ packages/api/src/api/plugins/cors.ts
@@ -1,14 +1,8 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
-import { env } from "process";
 
 export function corsPlugin() {
     return new Elysia({ name: "cors" }).use(
-        cors({
-            origin:
-                env.NODE_ENV === "production"
-                    ? env.ITAM_EDU_FRONTEND_URL!
-                    : () => true
-        }).as("scoped")
+        cors({ origin: true }).as("scoped")
     );
 }
