8a33213c2f9b5aa0e15bdcbc96545229e6bf5996
ClayenKitten <me@clayenkitten.dev>
2025-09-25 22:20:11 +0000

    feat(frontend): remove course theme configuration
    Backend and some machinery for theme preview stay in place, at least for now. We will decide the final fate of the feature later.

--- packages/frontend/src/lib/types.ts
+++ packages/frontend/src/lib/types.ts
@@ -22,11 +22,15 @@ import type { StaffMemberDto } from "itam-edu-api/src/courses/staff/schema";
 import type { InviteDto } from "itam-edu-api/src/courses/staff/invites/query";
 import type { StudentDto } from "itam-edu-api/src/courses/student/schema";
 import type { AttendeeDto } from "itam-edu-api/src/courses/lesson/attendance/query";
-import type { CreateCourseDto } from "itam-edu-api/src/courses/schema";
+import type {
+    CreateCourseDto,
+    UpdateCourseDto
+} from "itam-edu-api/src/courses/schema";
 
 export type Course = CourseQueryDto;
 export type CoursePartial = CourseQueryPartialDto;
 export type CreateCourse = CreateCourseDto;
+export type UpdateCourse = UpdateCourseDto;
 export type CourseChange = CourseChangeDto;
 
 export type StaffMember = StaffMemberDto;
--- packages/frontend/src/routes/c/[year]/[semester=season]/[slug]/settings/StyleSection.svelte
+++ packages/frontend/src/routes/c/[year]/[semester=season]/[slug]/settings/StyleSection.svelte
@@ -2,12 +2,9 @@
     import { invalidateAll } from "$app/navigation";
     import api, { UploadClient } from "$lib/api";
     import ImageUploader from "$lib/components/upload/ImageUploader.svelte";
-    import { getColors, themes, type Theme } from "$lib/metadata";
     import { filePath } from "$lib/path";
     import { getToaster } from "$lib/Toaster.svelte";
-    import type { Course } from "$lib/types";
-
-    import { getContext } from "svelte";
+    import type { Course, UpdateCourse } from "$lib/types";
 
     let { readonly, course }: Props = $props();
     type Props = {
@@ -16,54 +13,35 @@
     };
     const toaster = getToaster();
 
-    const courseClone = $state(structuredClone($state.snapshot(course)));
-    const themeOverride = getContext<{ theme: Theme | null }>("themeOverride");
-    $effect(() => {
-        themeOverride.theme = courseClone.theme as Theme;
-    });
-
-    let uploadCover: File | null | undefined = $state();
-    let uploadIcon: File | null | undefined = $state();
-    let uploadBanner: File | null | undefined = $state();
-
-    async function save() {
-        const fileClient = new UploadClient({ fetch });
-
-        const [cover, icon, banner] = await Promise.all([
-            uploadCover !== undefined
-                ? uploadCover === null
-                    ? null
-                    : fileClient.uploadCourseFile(
-                          course.id,
-                          "cover",
-                          uploadCover
-                      )
-                : courseClone.cover,
-            uploadIcon !== undefined
-                ? uploadIcon === null
-                    ? null
-                    : fileClient.uploadCourseFile(course.id, "icon", uploadIcon)
-                : courseClone.icon,
-            uploadBanner !== undefined
-                ? uploadBanner === null
-                    ? null
-                    : fileClient.uploadCourseFile(
-                          course.id,
-                          "banner",
-                          uploadBanner
-                      )
-                : courseClone.banner
-        ]);
-
-        const result = await api({ fetch })
-            .courses({ course: courseClone.id })
-            .patch({ theme: courseClone.theme, cover, icon, banner });
-        if (result.error) {
-            toaster.add("Не удалось сохранить изменения", "error");
+    async function save(field: "cover" | "icon" | "banner", file: File | null) {
+        try {
+            let update: Partial<UpdateCourse> = {};
+            if (file !== null) {
+                const fileClient = new UploadClient({ fetch });
+                const filename = await fileClient.uploadCourseFile(
+                    course.id,
+                    field,
+                    file
+                );
+                update[field] = filename;
+            } else {
+                update[field] = null;
+            }
+            const result = await api({ fetch })
+                .courses({ course: course.id })
+                .patch(update);
+            if (result.error) {
+                toaster.add("Не удалось сохранить файл", "error");
+                return;
+            }
+            toaster.add(
+                "Файл сохранён, обновите страницу, чтобы увидеть изменения"
+            );
+        } catch {
+            toaster.add("Не удалось сохранить файл", "error");
             return;
         }
         await invalidateAll();
-        toaster.add("Изменения сохранены");
     }
 </script>
 
@@ -71,39 +49,6 @@
     <header>
         <h3>Стилизация</h3>
     </header>
-    <div class="flex-1 min-w-max flex flex-col gap-2">
-        <h4>Цветовая палитра</h4>
-        <ul
-            class={[
-                "grid grid-cols-[repeat(auto-fit,60px)] auto-rows-[60px]",
-                "gap-2 content-center items-center"
-            ]}
-        >
-            {#each themes as theme (theme)}
-                {@const colors = getColors(theme)}
-                <button
-                    class={[
-                        "self-center justify-self-center",
-                        "flex justify-center items-center",
-                        "transition-all duration-200",
-                        "size-12 border rounded-full",
-                        "bg-conic from-primary to-on-primary from-50% to-50%",
-                        "border-primary-border opacity-60",
-                        !readonly && "hover:opacity-100 cursor-pointer",
-                        theme === courseClone.theme &&
-                            "border-primary opacity-100 size-15"
-                    ]}
-                    style:--color-primary={colors.primary}
-                    style:--color-primary-border={colors.primaryBorder}
-                    style:--color-on-primary={colors.onPrimary}
-                    onclick={() => (courseClone.theme = theme)}
-                    disabled={readonly}
-                    aria-label={`Тема "${theme}"`}
-                >
-                </button>
-            {/each}
-        </ul>
-    </div>
     <div class="flex flex-wrap gap-y-6 gap-x-12">
         <div class="w-max flex flex-col items-start gap-2">
             <header>
@@ -112,15 +57,13 @@
                     class="w-[400px] text-md-regular text-on-surface-muted text-balance"
                 >
                     Обложка отображается в списке курсов, в предпросмотре ссылок
-                    в Telegram и так далее. Рекомендуется изображение размером
-                    от 315x315.
+                    в Telegram и тд. Рекомендуется изображение размером 315x315
+                    пикселей.
                 </p>
             </header>
             <ImageUploader
-                url={courseClone.cover ? filePath(courseClone.cover) : null}
-                onChange={file => {
-                    uploadCover = file;
-                }}
+                url={course.cover ? filePath(course.cover) : null}
+                onChange={file => save("cover", file)}
                 height="315px"
                 aspectRatio="315/315"
                 {readonly}
@@ -133,14 +76,13 @@
                     class="w-[400px] text-md-regular text-on-surface-muted text-balance"
                 >
                     Иконка отображается в селекторе курсов и около вкладки в
-                    браузере. Рекомендуется изображение размером 128x128.
+                    браузере. Рекомендуется круглое изображение размером 128x128
+                    пикселей.
                 </p>
             </header>
             <ImageUploader
-                url={courseClone.icon ? filePath(courseClone.icon) : null}
-                onChange={file => {
-                    uploadIcon = file;
-                }}
+                url={course.icon ? filePath(course.icon) : null}
+                onChange={file => save("icon", file)}
                 height="128px"
                 aspectRatio="1/1"
                 {readonly}
@@ -153,21 +95,14 @@
             <p class="max-w-[600px] text-md-regular text-on-surface-muted">
                 Баннер отображается на главной странице курса и в разделе "О
                 курсе". <br />
-                Рекомендуется изображение размером не менее 1600x320.
+                Рекомендуется изображение размером 1600x320 пикселей.
             </p>
         </header>
         <ImageUploader
-            url={courseClone.banner ? filePath(courseClone.banner) : null}
-            onChange={file => {
-                uploadBanner = file;
-            }}
+            url={course.banner ? filePath(course.banner) : null}
+            onChange={file => save("banner", file)}
             aspectRatio="5/1"
             {readonly}
         />
     </div>
-    {#if !readonly}
-        <footer class="flex justify-end gap-4">
-            <button class="btn w-max" onclick={save}>Сохранить</button>
-        </footer>
-    {/if}
 </section>
