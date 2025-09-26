10ea9f8e64f8715dc736c154cdd28e027834bdfe
ClayenKitten <me@clayenkitten.dev>
2025-09-26 06:29:29 +0000

    feat(frontend): and back button from homework page to homeworks list

--- packages/frontend/src/routes/c/[year]/[semester=season]/[slug]/homeworks/[homeworkId]/+page.svelte
+++ packages/frontend/src/routes/c/[year]/[semester=season]/[slug]/homeworks/[homeworkId]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { coursePath } from "$lib/path";
     import HomeworkInfoSection from "./HomeworkInfoSection.svelte";
     import SubmissionAttempt from "./SubmissionAttempt.svelte";
 
@@ -11,12 +12,21 @@
         "max-w-[1000px] @min-[1200px]/main:mx-40"
     ]}
 >
-    <HomeworkInfoSection
-        user={data.user}
-        course={data.course}
-        homework={data.homework}
-        submission={data.submission}
-    />
+    <div class="flex flex-col gap-5">
+        <a
+            class="group self-start flex items-center h-min gap-2 text-primary"
+            href="{coursePath(data.course)}/homeworks"
+        >
+            <i class="ph ph-caret-left text-[20px]"></i>
+            <h5 class="group-hover:underline">К списку заданий</h5>
+        </a>
+        <HomeworkInfoSection
+            user={data.user}
+            course={data.course}
+            homework={data.homework}
+            submission={data.submission}
+        />
+    </div>
     {#if data.submission}
         <ol class="flex flex-col gap-5">
             {#each data.submission.attempts as attempt, i (attempt.id)}
