59ed61e26d69de5564a2d67a5b6c1a2a7c949a39
AlexThatDefinetlyStudiesIT <m2208273@edu.misis.ru>
2025-10-07 14:16:49 +0000

    feat: send lesson date change notifications only if within last hour

--- packages/api/src/features/courses/lesson/update.interactor.ts
+++ packages/api/src/features/courses/lesson/update.interactor.ts
@@ -9,6 +9,7 @@ import { CourseChangelog } from "../changes";
 import { CourseRepository } from "../repository";
 import { NotificationTemplate } from "../../notifications";
 import type { Course } from "../entity";
+import logger from "../../../logger";
 
 @injectable()
 export class UpdateLesson {
@@ -69,16 +70,19 @@ export class UpdateLesson {
         );
         await this.repo.save(newLesson);
 
+        const oneHourAgo = new Date(new Date().getTime() - 60 * 60 * 1000);
         if (change.schedule !== undefined) {
             if (newLesson.schedule !== null) {
-                await this.notificationSender.send(
-                    new Notification(course, newLesson),
-                    [...course.members.map(m => m.id)]
-                );
-                await this.changelog.add(actor, course, {
-                    kind: "lesson-schedule-changed",
-                    lessonId: lesson.id
-                });
+                if (new Date(newLesson.schedule.date) >= oneHourAgo) {
+                    await this.notificationSender.send(
+                        new Notification(course, newLesson),
+                        [...course.members.map(m => m.id)]
+                    );
+                    await this.changelog.add(actor, course, {
+                        kind: "lesson-schedule-changed",
+                        lessonId: lesson.id
+                    });
+                }
             }
         }
 
